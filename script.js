19ac1ee33bbc2a136dbee9f28e734ed5a41468c2
SteveStar <71157526+SteveStar@users.noreply.github.com>
2024-11-21 19:08:43 -0500

    Added the third part

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//First Part
 var hoverDiv = document.getElementById('hoverDiv');
 
 hoverDiv.addEventListener('mouseenter', function() {
@@ -8,11 +9,28 @@ hoverDiv.addEventListener('mouseleave', function() {
     this.style.backgroundColor = 'transparent';
 });
 
-
+//Second Part
 const inputField = document.getElementById('keyInput');
 const outputField = document.getElementById('keyOutput');
 
 inputField.addEventListener('keydown', (event) => {
-  // Display the latest key pressed
+  
   outputField.textContent = `Latest key pressed: ${event.key}`;
 });
+
+//Third Part
+const form = document.getElementById('formEvents');
+const messageDiv = document.getElementById('message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const firstName = form.firstNameForm.value; 
+  const lastName = form.lastNameForm.value; 
+
+  messageDiv.textContent = `Hello, ${firstName} ${lastName}!`;
+  
+  form.reset();
+});
+
+//fourth part
